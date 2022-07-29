fc54ac0ef51f171d506ace7b3b20c55a106cb200
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-07-29 21:41:10 +0530

    Remove last n characters of string.js

--- Javascript Basics/Remove last n characters of string.js
+++ Javascript Basics/Remove last n characters of string.js
@@ -1,1 +1,6 @@
-a.slice(0, -3);
+// Write a function that takes a string (a) as argument
+// Remove the last 3 characters of a
+// Return the result
+function myFunction(a) {
+  return a.slice(0, -3);
+}

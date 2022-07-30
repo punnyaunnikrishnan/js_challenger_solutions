22c35687feb0b8a7276f5f1f29b6adcb38393e48
Punnya Unnikrishnan P <punnyaunnikrishnan1995@gmail.com>
2022-07-30 22:04:48 +0530

    Insert character after every n characters (backwards).js

---
+++ Javascript Basics/Insert character after every n characters (backwards).js
@@ -0,0 +1,9 @@
+function myFunction(a, b) {
+  let result = [];
+  let rest = a;
+  while (rest.length) {
+    result.push(rest.slice(-3));
+    rest = rest.slice(0, -3);
+  }
+  return result.reverse().join(b);
+}
